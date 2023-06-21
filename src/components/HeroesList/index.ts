cf39a3673c5b7b1ee23a39bafecea85ebb5ecdb6
brunopeople <brunopessoa.melo@gmail.com>
2023-06-21 17:32:40 -0300

    adicionando diretório de fonte e futuramente criando a lista de herois

---
+++ src/components/HeroesList/index.ts
@@ -0,0 +1,14 @@
+import {IHeroData} from "@/interfaces/heroes";
+
+interface IProps{
+    heroes: IHeroData[];
+
+}
+
+export default function HeroesList({heroes}: IProps) {
+    return(
+        <>
+            <h1>Personagens</h1>
+        </>
+    );
+}
